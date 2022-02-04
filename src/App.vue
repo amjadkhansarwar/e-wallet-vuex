f2e351592d04513a2b92148691b8193d026a9d11
amjad <amjadkhansarwar@gmail.com>
2022-02-04 16:02:35 +0100

    this is first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link> ||
       <router-link to="/about">About</router-link>
     </div>
+  <h1>
+  this is a home page
+  </h1>
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  data(){
+
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
